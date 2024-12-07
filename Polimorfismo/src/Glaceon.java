1174578ec5abdcfe8b20aa7bfa137b8bb40f06a1
Matheus Filipe <math.filipe23@gmail.com>
2024-12-07 16:35:33 -0300

    Create Glaceon.java

---
+++ Polimorfismo/src/Glaceon.java
@@ -0,0 +1,18 @@
+public class Glaceon extends Eevee{
+
+    @Override
+    public String ataque(){
+        return "Raio de Gelo - Ice Beam";
+    }
+
+    @Override
+    public String defesa(){
+        return "Nevasca - Blizzard";
+    }
+
+    @Override
+    public String especial(){
+        return "Vento Congelante - Icy Wind"; 
+    }
+
+}
