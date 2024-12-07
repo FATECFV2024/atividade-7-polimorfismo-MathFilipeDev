484a476d9caca05ea9d97a28dae0100698982dce
Matheus Filipe <math.filipe23@gmail.com>
2024-12-07 16:30:02 -0300

    Create Espeon.java

---
+++ Polimorfismo/src/Espeon.java
@@ -0,0 +1,18 @@
+public class Espeon extends Eevee {
+
+    @Override
+    public String ataque(){
+        return "Psíquico - Psychic";
+    }
+
+    @Override
+    public String defesa(){
+        return "Bola Sombria - Shadow Ball";
+    }
+
+    @Override
+    public String especial(){
+        return "Onda Psíquica - Psybeam"; 
+    }
+
+}
