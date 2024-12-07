e6ae6706016c57f6de4363e7ac7d38ff2228336b
Matheus Filipe <math.filipe23@gmail.com>
2024-12-07 16:34:03 -0300

    Create Leafeon.java

---
+++ Polimorfismo/src/Leafeon.java
@@ -0,0 +1,18 @@
+public class Leafeon extends Eevee{
+
+    @Override
+    public String ataque(){
+        return "Lâmina de Folha - Lead Blade";
+    }
+
+    @Override
+    public String defesa(){
+        return "Raio Solar - Solar Beam";
+    }
+
+    @Override
+    public String especial(){
+        return "Folha Mágica - Magical Leaf"; 
+    }
+
+}
