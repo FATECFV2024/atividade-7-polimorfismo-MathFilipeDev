165a19498abff73df0280477a4febab0f9494dc6
Matheus Filipe <math.filipe23@gmail.com>
2024-12-07 16:31:48 -0300

    Create Umbreon.java

---
+++ Polimorfismo/src/Umbreon.java
@@ -0,0 +1,18 @@
+public class Umbreon extends Eevee{
+
+    @Override
+    public String ataque(){
+        return "Pulso das Trevas - Dark Pulse";
+    }
+
+    @Override
+    public String defesa(){
+        return "Ataque Furtivo - Faint Attack";
+    }
+
+    @Override
+    public String especial(){
+        return "Jogo Sujo - Foul Play"; 
+    }
+
+}
