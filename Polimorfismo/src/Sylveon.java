43da52ad355cb8952b067e5a56f37d22e9519077
Matheus Filipe <math.filipe23@gmail.com>
2024-12-07 16:37:26 -0300

    Create Sylveon.java

---
+++ Polimorfismo/src/Sylveon.java
@@ -0,0 +1,18 @@
+public class Sylveon extends Eevee{
+
+    @Override
+    public String ataque(){
+        return "Brilho Deslumbrante - Dazzling Gleam";
+    }
+
+    @Override
+    public String defesa(){
+        return "Encanto - Charm";
+    }
+
+    @Override
+    public String especial(){
+        return "Explosão Lunar - Moonblast"; 
+    }
+
+}
