19b2321269c3c57431e08e65d486f4dfd8fc2cb1
Matheus Filipe <math.filipe23@gmail.com>
2024-12-07 16:45:02 -0300

    Update Pokedex.java

--- Polimorfismo/src/Pokedex.java
+++ Polimorfismo/src/Pokedex.java
@@ -2,6 +2,13 @@ public class Pokedex {
     public static void main(String[] args) throws Exception {
         Eevee e1 = new Eevee();
         Jolteon j1 = new Jolteon();
+        Espeon e2 = new Espeon();
+        Flareon f1 = new Flareon();
+        Glaceon g1 = new Glaceon();
+        Leafeon l1 = new Leafeon();
+        Sylveon s1 = new Sylveon();
+        Umbreon u1 = new Umbreon();
+        Vaporeon v1 = new Vaporeon(); 
 
         System.out.println("Ataque: "+e1.ataque());
         System.out.println("Defesa: "+e1.defesa());
