a6bbf3efc964b1d5151219fc96bf3c23b88680fc
Matheus Filipe <math.filipe23@gmail.com>
2024-12-07 16:28:15 -0300

    Create Flareon.java

---
+++ Polimorfismo/src/Flareon.java
@@ -0,0 +1,18 @@
+public class Flareon extends Eevee{
+
+    @Override
+    public String ataque(){
+        return "Lança-chamas - Flamethrower";
+    }
+
+    @Override
+    public String defesa(){
+        return "Giro de fogo - Fire Spin";
+    }
+
+    @Override
+    public String especial(){
+        return "Superaquecimento - Overheat"; 
+    }
+
+}
