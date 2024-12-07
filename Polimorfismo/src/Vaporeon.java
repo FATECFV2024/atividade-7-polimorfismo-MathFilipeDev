af8fb799c3bf3a2c4472f2ba6496c3b6f4dee6ad
Matheus Filipe <math.filipe23@gmail.com>
2024-12-07 16:26:12 -0300

    Create Vaporeon.java

---
+++ Polimorfismo/src/Vaporeon.java
@@ -0,0 +1,18 @@
+public class Vaporeon extends Eevee {
+
+    @Override
+    public String ataque(){
+        return "Cauda da Água - Aqua Tail";
+    }
+
+    @Override
+    public String defesa(){
+        return "Jato da Água - Hydro Pump";
+    }
+
+    @Override
+    public String especial(){
+        return "Névoa de Gelo - Ice Beam"; 
+    }
+
+}
